ab4dfc3d732e852b764a9d610c290c1b9e7a9f63
omegamotor <omegamotor1997@gmail.com>
2019-12-25 20:10:42 +0100

    Drobne zmiany

--- quiz/userProfile/urls.py
+++ quiz/userProfile/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from userProfile import views
+from userProfile.views import login
 
 urlpatterns = [
     path('', views.profile, name='user_profile'),
+    path('login/', login),
 ]
